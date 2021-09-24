26e1c10be0b4aac178bd5af55b36c559812de534
Jorge Manzur <46169161+manzur1990@users.noreply.github.com>
2021-09-23 19:44:14 -0500

    handleAddToCart & handleRemoveFromCart integrated to allow adding and removal of item cart

--- src/App.tsx
+++ src/App.tsx
@@ -40,9 +40,34 @@ const App = () => {
   const getTotalItems = (items: CartItemType[]) => 
     items.reduce((acc: number, item) => acc + item.amount, 0);
 
-  const handleAddToCart = (clickedItem: CartItemType) => null;
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    setCartItems(prev => {
+      // 1. Is the item already in the cart?
+      const isItemInCart = prev.find(item => item.id === clickedItem.id)
+      if(isItemInCart) {
+        return prev.map(item => (
+          item.id === clickedItem.id
+            ? { ...item, amount: item.amount + 1}
+            : item
+        ));
+      }
+      // First time the item is added
+      return [ ...prev, {...clickedItem, amount: 1}]
+    });
+  };
 
-  const handleRemoveFromCart = () => null; 
+  const handleRemoveFromCart = (id: number) => {
+    setCartItems(prev => 
+      prev.reduce((acc, item) => {
+        if(item.id === id){
+          if (item.amount === 1) return acc
+          return [ ...acc, {...item, amount: item.amount - 1}]
+        } else {
+          return [ ...acc, item]
+        }
+      }, [] as CartItemType[])
+    );
+  }; 
 
   if (isLoading) return <LinearProgress />;
   if (error) return <div>Something went wrong ...</div>
